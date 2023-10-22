cd93bb1884709ad855dce8a52b204757999385ce
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2023-10-23 02:32:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from nltk.tokenize import word_tokenize
 import os  # Added for file path handling
 import openai
 
-openai.api_key = "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"
+openai.api_key = api_key
 
 # Download the necessary NLTK datasets
 nltk.download('punkt')
