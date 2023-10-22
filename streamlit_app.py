181f44061fe22fa4f4ddceb7bab8c5ee32612733
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2023-10-23 01:41:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
-from sklearn.preprocessing import StandardScaler
-from statsmodels.compat.pandas import Appender
-import streamlit as st
-import pickle
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import os  # Added for file path handling
-import openai
+import streamlit as st
+import os
+import pickle
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
-
-# Download the necessary NLTK datasets
-nltk.download('punkt')
-nltk.download('stopwords')
-
-# Function to preprocess data
-def preprocess_data(df):
-    # Handle missing values by forward filling
-    df = df.fillna(method='ffill')
-
-    # Optionally, handle any remaining missing values by backward filling
-    df = df.fillna(method='bfill')
-
-    # Optionally, standardize (scale) the data to have zero mean and unit variance
-    scaler = StandardScaler()
-    df[df.columns] = scaler.fit_transform(df[df.columns])
-
-    return df
+# Convert series to supervised learning
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = pd.DataFrame(data)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        names += [('var%d(t-%d)' % (j + 1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        if i == 0:
+            names += [('var%d(t)' % (j + 1)) for j in range(n_vars)]
+        else:
+            names += [('var%d(t+%d)' % (j + 1, i)) for j in range(n_vars)]
+    # put it all together
+    agg = pd.concat(cols, axis=1)
+    agg.columns = names
+    # drop rows with NaN values
+    if dropnan:
+        agg.dropna(inplace=True)
+    return agg
 
 def trading_advice(actual, prediction):
-    """
-    Provide trading advice based on prediction and actual prices.
-    """
     if prediction > actual:
         return "Based on the predictions, you might consider buying."
     elif prediction < actual:
@@ -46,116 +41,78 @@ def trading_advice(actual, prediction):
     else:
         return "The price seems stable. You might consider holding."
 
-def chatbot_response(user_input, predefined_responses):
-    # Tokenize and vectorize user input and predefined responses
-    vectorizer = TfidfVectorizer(tokenizer=lambda text: nltk.word_tokenize(text, language='english'),
-                                 stop_words=stopwords.words('english'))
-    vectors = vectorizer.fit_transform([user_input] + predefined_responses)
-
-    # Calculate cosine similarities
-    cosine_matrix = cosine_similarity(vectors)
-
-    # Find the most similar predefined response to the user's input
-    response_idx = np.argmax(cosine_matrix[0][1:])
-
-    return predefined_responses[response_idx]
-
-def get_gpt3_response(user_input):
-    prompt = f"Answer questions related to cryptocurrency.\n\nUser: {user_input}\nBot:"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=50  # You can adjust this based on response length
-    )
-    return response.choices[0].text
-
-
 # Streamlit app
 def main():
     st.title("Cryptocurrency Price Prediction and Trading Bot")
 
-    # Specify the path to the CSV file
-    csv_path = 'https://github.com/Surya152002/Intel-Task-8/blob/main/crypto_dataset%20(9).csv'  # Update with your specific file path
-
-    # Check if the CSV file exists at the specified path
-    if not os.path.exists(csv_path):
-        st.error("The specified CSV file does not exist. Please provide a valid file path.")
-        return
-
+    csv_path = 'https://github.com/Surya152002/Intel-Task-8/blob/main/crypto_dataset%20(9).csv'
+    
     # Read and preprocess the input data
-    input_data = pd.read_csv(csv_path, parse_dates=['Timestamp'], index_col='Timestamp')
-    input_data = preprocess_data(input_data)
-
-    # List of coins
+    df = pd.read_csv(csv_path, parse_dates=['Timestamp'], index_col='Timestamp')
+    
     coins = ['BTC-USD Close', 'ETH-USD Close', 'LTC-USD Close']
-
-    # Get user's choice of cryptocurrency
     coin_choice = st.selectbox("Select a cryptocurrency", coins)
 
-    # Train ARIMA model if needed
     if coin_choice == "BTC-USD Close":
-        coin_model_path = 'btc-usd_close_model.pkl'  # Update with your desired model path
-        coin_column = 'BTC-USD Close'
+        coin_model_path = 'btc-usd_close_model.pkl'
     elif coin_choice == "ETH-USD Close":
-        coin_model_path = 'eth-usd_close_model.pkl'  # Update with your desired model path
-        coin_column = 'ETH-USD Close'
+        coin_model_path = 'eth-usd_close_model.pkl'
     else:
-        coin_model_path = 'ltc-usd_close_model.pkl'  # Update with your desired model path
-        coin_column = 'LTC-USD Close'
+        coin_model_path = 'ltc-usd_close_model.pkl'
+    
+    values = df[coin_choice].values.reshape(-1, 1)
+    
+    # Normalize features
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
 
-    # Check if the model exists, if not, train it
+    # Frame as supervised learning
+    reframed = series_to_supervised(scaled, 1, 1)
+    
+    # Split into train and test sets
+    values = reframed.values
+    n_train = int(len(values) * 0.8)
+    train, test = values[:n_train, :], values[n_train:, :]
+
+    # Split into input and output
+    train_X, train_y = train[:, :-1], train[:, -1]
+    test_X, test_y = test[:, :-1], test[:, -1]
+
+    # Reshape for LSTM [samples, timesteps, features]
+    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+
+    # LSTM model
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mae', optimizer='adam')
+
+    # Check if model exists, if not, train it
     if not os.path.exists(coin_model_path):
-        # Split the data into training and testing sets
-        train_size = int(len(input_data) * 0.8)
-        train, test = input_data[:train_size], input_data[train_size:]
+        # Fit the model
+        model.fit(train_X, train_y, epochs=50, batch_size=72, verbose=0, shuffle=False)
 
-        # Build and train the ARIMA model
-        model = ARIMA(train[coin_column], order=(5, 1, 0))  # Example order, you can tune this
-        model_fit = model.fit()
+        # Save model
+        model.save(coin_model_path)
+    else:
+        # Load the model
+        model = keras.models.load_model(coin_model_path)
+    
+    # Predict on the entire data
+    yhat = model.predict(test_X)
+    test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
-        # Save the trained model using pickle
-        with open(coin_model_path, 'wb') as model_file:
-            pickle.dump(model_fit, model_file)
-
-    # Load the model using pickle
-    with open(coin_model_path, 'rb') as model_file:
-        model_fit = pickle.load(model_file)
-
-    # Make predictions
-    predictions = model_fit.forecast(steps=len(input_data))
+    # Invert scaling
+    inv_yhat = np.concatenate((yhat, test_X[:, 1:]), axis=1)
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:, 0]
 
     # Display predictions
     st.header("Price Predictions")
-    st.write(predictions)
+    st.write(inv_yhat)
 
-    # Visualize results
-    st.header("Price Prediction Chart")
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(input_data.index, input_data[coin_column], label='Actual')
-    ax.plot(input_data.index, predictions, label='Predicted', linestyle='--')
-    ax.set_title(f'{coin_choice} Price Prediction')
-    ax.legend()
-    st.pyplot(fig)
-
-    # Chatbot live interaction
-
-    st.header("Chat with Trading Bot")
-
-    user_message = st.text_input("You: ")
-    predefined_responses = [
-        "The predicted price for the next period is {}.".format(predictions.iloc[-1]),
-        trading_advice(input_data[coin_column].iloc[-1], predictions.iloc[-1]),
-        "I am here to help with your cryptocurrency trading decisions.",
-        "Can you specify your query?"
-    ]
-
-    if user_message:
-        if "predict" in user_message.lower() or "forecast" in user_message.lower():
-            bot_reply = trading_advice(input_data[coin_column].iloc[-1], predictions.iloc[-1])
-        else:
-            bot_reply = get_gpt3_response(user_message)
-        st.write(f"Bot: {bot_reply}")
+    # TODO: Rest of the streamlit interface
 
 if __name__ == "__main__":
     main()
-
