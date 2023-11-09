87aa09780ff4c24d4a83a55b0c2064fe5962003f
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2023-11-09 14:35:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,101 +1,129 @@
 import streamlit as st
 import yfinance as yf
-import backtrader as bt
 import pandas as pd
-from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import load_model
+import plotly.graph_objs as go
 import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime
+import base64
 
-# Load your trained LSTM model
-model = load_model('./best_model.h5')  # Uncomment this line to load your trained model
-
-# Define the LSTM prediction function
-def lstm_predict(model, data, scaler, look_back=60):
-    # Preprocess data
-    last_values = scaler.transform(data[-look_back:].reshape(-1, 1))
-    X_test = np.reshape(last_values, (1, look_back, 1))
+# Function to make predictions using the LSTM model
+def make_predictions(model, data, scaler, look_back, prediction_days):
+    predictions = []
+    scaled_data = scaler.transform(data[-look_back:].reshape(-1, 1))
+    scaled_data = scaled_data.reshape((1, look_back, 1))
     
-    # Make prediction
-    predicted_price = model.predict(X_test)
-    predicted_price = scaler.inverse_transform(predicted_price)
-    return predicted_price[0, 0]
-
-# Define the trading strategy using LSTM predictions
-class LSTMStrategy(bt.Strategy):
-    params = dict(
-        model=None,  # LSTM model
-        scaler=None,  # Scaler object for data normalization
-        look_back=60  # Look back period for LSTM input
-    )
-
-    def __init__(self):
-        self.order = None
-        self.price = self.datas[0].close
-        self.predicted_price = None
-
-    def next(self):
-        if self.order:
-            return  # pending order execution
+    for _ in range(prediction_days):
+        # Make the prediction
+        predicted_price = model.predict(scaled_data)
+        # Inverse transform to get the actual price
+        predicted_price = scaler.inverse_transform(predicted_price)
+        predictions.append(predicted_price.ravel()[0])
         
-        if not self.position:  # if not in the market
-            # Predict the next closing price
-            self.predicted_price = lstm_predict(
-                self.params.model,
-                np.array(self.price.get(size=self.params.look_back)),
-                self.params.scaler
-            )
-            if self.predicted_price > self.price[0]:  # if the price is predicted to rise
-                self.order = self.buy()
-        else:
-            if self.predicted_price < self.price[0]:  # if the price is predicted to fall
-                self.order = self.sell()
+        # Append prediction to scaled_data for making subsequent predictions
+        next_input = scaler.transform(predicted_price.reshape(-1, 1))
+        scaled_data = np.append(scaled_data[:, 1:, :], [next_input], axis=1)
+        
+    return predictions
 
-# Function to fetch data
-def get_data(ticker, start_date, end_date):
-    data = yf.download(ticker, start=start_date, end=end_date)
-    return data
 
-# Function to run backtest
-def run_backtest(data, strategy, model, scaler):
-    cerebro = bt.Cerebro()
-    feed = bt.feeds.PandasData(dataname=data)
-    cerebro.adddata(feed)
-    cerebro.addstrategy(strategy, model=model, scaler=scaler)
-    cerebro.broker.set_cash(10000)
-    cerebro.run()
-    return cerebro
+# Function to download prediction data as a CSV file
+def get_table_download_link(df):
+    csv = df.to_csv(index=True)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="predictions.csv">Download CSV file</a>'
+    return href
 
-# Streamlit interface
-st.title('Cryptocurrency Trading Bot with LSTM')
 
-# User inputs for the prediction and backtesting
+# Streamlit app layout
+st.title('Real-time Crypto Prediction with LSTM')
+
+# User input parameters (no longer in the sidebar)
+st.header('User Input Parameters')
 ticker = st.selectbox('Select Ticker', ('BTC-USD', 'ETH-USD', 'LTC-USD'))
-start_date = st.date_input('Start Date', value=pd.to_datetime('2021-01-01'))
-end_date = st.date_input('End Date', value=pd.to_datetime('2021-12-31'))
-look_back = st.slider('Look Back Period', min_value=10, max_value=100, value=60)
+start_date = st.date_input('Start date', pd.to_datetime('2021-01-01'))
+end_date = st.date_input('End date', pd.to_datetime('today'))
+look_back = st.number_input('Look Back Period',value=60)
+prediction_days = st.slider('Days to Predict', 1, 30, 5)
 
-# Button to make prediction and run the strategy
-if st.button('Run Trading Strategy'):
-    data = get_data(ticker, start_date, end_date)
-    scaler = MinMaxScaler(feature_range=(0, 1))  # Initialize the MinMaxScaler
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+# Load pre-trained LSTM model and scaler
+with st.spinner('Loading model and scaler...'):
+    model = load_model('best_model.h5')  # make sure to use the correct path to your model
+    scaler = MinMaxScaler(feature_range=(0, 1))  # Assuming the scaler was fitted to the training data
 
-    # Mock model prediction (replace this with actual model.predict call)
-    # For demonstration, we assume next price is same as last closing price
-    next_price = data['Close'].iloc[-1]
+# Fetch historical data from yfinance
+with st.spinner(f'Downloading {ticker} data...'):
+    data = yf.download(ticker, start=start_date, end=end_date)
+st.success('Downloaded data successfully!')
+
+# Prepare data for prediction
+data_close = data['Close'].values.reshape(-1, 1)
+scaler.fit(data_close)
+
+# Make real-time prediction
+if len(data_close) >= look_back:
+    with st.spinner('Making predictions...'):
+        predictions = make_predictions(model, data_close, scaler, look_back, prediction_days)
+        # Convert predictions to a list of Python floats
+        predictions_list = [float(pred) for pred in predictions]
+    st.success('Predictions made successfully!')
+
+    # Plot the results using Plotly
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines+markers', name='Close Price'))
+    # Add predicted prices to the plot
+    prediction_dates = pd.date_range(start=data.index[-1], periods=prediction_days+1, closed='right')
+    fig.add_trace(go.Scatter(x=prediction_dates, y=predictions_list, mode='lines+markers', name='Predicted Price', marker=dict(color='red')))
+    fig.update_layout(title='Cryptocurrency Price Prediction', xaxis_title='Date', yaxis_title='Price', legend_title='Legend')
+    st.plotly_chart(fig, use_container_width=True)
+
     
-    # Display the prediction
-    st.write(f'The predicted next closing price is ${next_price}')
 
-    # Run backtest with LSTM Strategy
-    cerebro = run_backtest(data, LSTMStrategy, model=None, scaler=scaler)  # Pass the actual model instead of None
 
-    # Plot the results
-    fig, ax = plt.subplots()
-    ax.plot(data['Close'], label='Close Price')
-    st.pyplot(fig)
+    # Display download link for predictions with actual prices included
+    prediction_dates = pd.date_range(start=data.index[-1], periods=prediction_days+1, closed='right')
+    prediction_df = pd.DataFrame(index=prediction_dates)
+    prediction_df['Predicted Price'] = predictions_list
+    prediction_df['Actual Price'] = data['Close'][-prediction_days:].values
+    st.markdown(get_table_download_link(prediction_df), unsafe_allow_html=True)
+
+    # Display the last prediction
+    st.write(f'Last Predicted Closing Price for {ticker}: ${predictions_list[-1]:.2f}')
     
-    # Show the performance
-    st.write(f'Final Portfolio Value: ${cerebro.broker.getvalue():,.2f}')
+    # Simulate investment results based on predictions
+    # Simulate investment results based on the last prediction
+    initial_account_balance = 1000000  
+    account_balance_predicted = initial_account_balance
+    account_balance_actual = initial_account_balance
+
+    # Get the last actual closing price and the last predicted price
+    last_invest_price = data['Close'].iloc[-prediction_days-1]  # Price at which we "buy"
+    last_sell_price_predicted = predictions_list[-1]  # Predicted price at which we "sell"
+    last_sell_price_actual = data['Close'].iloc[-prediction_days] if prediction_days <= len(data['Close']) else last_sell_price_predicted
+
+    # Update account balances based on the last day's prediction and actual price
+    account_balance_predicted += (last_sell_price_predicted - last_invest_price) * (account_balance_predicted / last_invest_price)
+    account_balance_actual += (last_sell_price_actual - last_invest_price) * (account_balance_actual / last_invest_price)
+
+
+    # Assuming investing at the closing price and selling at the next day's predicted/actual price
+    for i in range(1, prediction_days + 1):
+        invest_price = data['Close'].iloc[-i-1]  # Price at which we "buy"
+        sell_price_predicted = predictions_list[-i]  # Predicted price at which we "sell"
+        sell_price_actual = data['Close'].iloc[-i] if i <= len(data['Close']) else sell_price_predicted
+
+        # Update account balances
+        account_balance_predicted += (sell_price_predicted - invest_price) * (account_balance_predicted / invest_price)
+        account_balance_actual += (sell_price_actual - invest_price) * (account_balance_actual / invest_price)
+
+    # Display simulated investment results
+    st.write(f"Initial Account Balance: ${initial_account_balance:,.2f}")
+    st.write(f"Account Balance based on Predictions: ${account_balance_predicted:,.2f}")
+    st.write(f"Account Balance based on Actual Prices: ${account_balance_actual:,.2f}")
+
+else:
+    st.error('Not enough data to make a prediction.')
+
+# Refresh the page to update the predictions
+if st.button("Refresh Predictions"):
+    st.experimental_rerun()
